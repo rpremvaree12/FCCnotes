f16c96b3ef506a6b6739ad62b1086508af4a4325
rpremvaree12 <richard.premvaree@gmail.com>
2023-02-19 19:44:39 -0500

    scatter plot

--- dataViz/d3scatterPlot/script.js
+++ dataViz/d3scatterPlot/script.js
@@ -1,8 +1,68 @@
 const DATAURL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
-let w = 500px;
-let h = 500px;
+let req = new XMLHttpRequest();
+
+let w = 500;
+let h = 500;
+let padding = 50;
+let xScale;
+let xAxisScale;
+let yScale;
+let yAxisScale;
+let xAxis;
+let yAxis;
+let dataset;
 
 let svg = d3.select("#chart-area")
-    .append("svg")
-    .attr("width",w)
-    .attr("height",h)
+    .append("svg");
+
+let drawCanvas = () => {
+    svg.attr("height", h)
+    .attr("width", w);
+}
+
+let generateScales = () =>{
+    let yearArray = dataset.map((d) => {
+        return new Date(d[0]); 
+    });
+
+    xAxisScale = d3.scaleTime()
+    .domain([d3.min(yearArray),d3.max(yearArray)])
+    .range([padding,w - padding]);
+
+    xScale = () =>{
+
+    }
+    yScale = () =>{
+
+    }
+    d3.axisBottom(xAxisScale);
+    svg.append("g")
+        .attr("id","x-axis")
+        .attr("transform", "translate(0," + (h - padding) + ")")
+        .call(xAxis)
+}
+
+let drawScales = () =>{
+
+}
+
+let drawPoints = () =>{
+    svg.selectAll("cirlce")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", w/2)
+    .attr("cy", h/2)
+    .attr('r',5)
+
+}
+
+req.open("GET",DATAURL,true);
+req.onload = () => {
+    dataset = JSON.parse(req.responseText);
+    drawCanvas();
+    generateScales();
+    drawScales();
+    drawPoints();
+}
+req.send();
